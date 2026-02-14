1524e561d932fc9ff6d46e133452c269c17cf401
SilenceMustBeHeard <spiritofsilence55@gmail.com>
2026-02-14 12:54:43 +0200

    global styles overhaul

--- FurnitureGardenDesign.Data/Migrations/20260214101521_InitialCreate.cs
+++ FurnitureGardenDesign.Data/Migrations/20260214101521_InitialCreate.cs
@@ -30,7 +30,8 @@ namespace FurnitureGardenDesign.Data.Migrations
                 columns: table => new
                 {
                     Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
-                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     UserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
                     NormalizedUserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
